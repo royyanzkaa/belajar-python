999cf5ae52d398fc17d678bba75c0a4979abbd87
Royyan Azka <royyan.musyaffa@student.alhazen.academy>
2023-11-10 17:13:21 +0700

    string color and emoji

---
+++ PIP Package Manager/main.py
@@ -0,0 +1,10 @@
+from stringcolor import *
+from emoji import *
+
+print(cs("Royyan Azka","blue"))
+print (bold("Selamat Datang"))
+print(cs("Royyan", "gold").underline())
+print(cs("Nama saya", "blue", "white"))
+
+print(emojize(":cold_face:"))
+print(emojize(":thumbs_up:"))
